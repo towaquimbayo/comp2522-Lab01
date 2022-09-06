799c43a995a4fbd248dbfa349c697ca786dde2fd
Towa Quimbayo <towacurtis@hotmail.com>
2022-09-06 09:32:38 -0700

    Second commit to Lab 01 - COMP 2522

--- src/main/java/comp2522/Main.java
+++
@@ -1,7 +0,0 @@
-package comp2522;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("I like listening to music!");
-    }
-}
---
+++ src/main/java/org/comp2522/Main.java
@@ -0,0 +1,10 @@
+package org.comp2522;
+
+/**
+ * Lab 01 Comp 2522 Main Class.
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("I like listening to music!");
+  }
+}
